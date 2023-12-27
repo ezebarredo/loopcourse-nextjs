82ef55fc98ddc6bd2c583df90098e6346a62e4b2
ezebarredo <ezequielbarredo@gmail.com>
2023-12-27 10:24:41 +0100

    menu level fixed

--- src/app/cards/main.js
+++ src/app/cards/main.js
@@ -2,7 +2,6 @@ import { level1_1 } from "../cards/level_1/set_1_1";
 import { level1_2 } from "../cards/level_1/set_1_2";
 import { level2_1 } from "../cards/level_2/set_2_1";
 
-// const level1Cards = [...level1_1, ...level1_2, ...level1_3, etc];
 const level1Cards = [...level1_1, ...level1_2];
 const level2Cards = [...level2_1];
 
